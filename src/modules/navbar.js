77a29857fd80aaedb7df8089b620f9ab8c0759d5
Burin2301 <burin2301@gmail.com>
2023-11-09 01:08:26 -0500

    small fixes

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -3,12 +3,12 @@ const header = document.createElement("div")
 const content = `
 <header>
 <div class="header-left">
-    <a href="#"><img class="header_logo" src="../src/assets/libro-de-recetas-freepik.png" alt=""></a>
+    <a href="#"><img class="header_logo" src="../assets/libro-de-recetas-freepik.png" alt=""></a>
     <a href="#" class="header_title">RECIPES :)</a>
 </div>
 
 <div class="burger_menu">
-    <img src="../src/assets/barra-de-menus (1).png" alt="burguerMenu">
+    <img src="../assets/barra-de-menus (1).png" alt="burguerMenu">
 </div>
 
 <ul class="header-list">
